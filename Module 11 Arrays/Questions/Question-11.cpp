4c571f5407c912d161bbb197e74b072fd04385f7
santwan <santwanpathak1@gmail.com>
2025-07-15 22:41:02 +0530

    Update Question-11.cpp

--- Module 11 Arrays/Questions/Question-11.cpp
+++ Module 11 Arrays/Questions/Question-11.cpp
@@ -1,3 +1,28 @@
 //! Reverse the Array without using any extra array
 //* Using Two Pointer
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr;
+    arr = {2,5,2,7,8,9,0,3,45,6};
+
+    int left = 0;
+    int right = arr.size() -1;
+
+    while(left<right){
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
+    }
+
+    cout<<"New Reverse Array: ";
+    for(int i=0 ; i<arr.size(); i++){
+        cout<<arr.at(i)<<" ";
+    }
+
+    return 0;
+
+    
+}
