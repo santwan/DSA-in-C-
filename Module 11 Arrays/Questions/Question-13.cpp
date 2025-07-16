20051fc7078cddd163c6670c89fbb192e32a3d77
santwan <santwanpathak1@gmail.com>
2025-07-16 13:09:04 +0530

    Create Question-13.cpp

---
+++ Module 11 Arrays/Questions/Question-13.cpp
@@ -0,0 +1,13 @@
+//! Rotate the given array by k steps , where k is non-negative. 
+//! k can be greater than the size of the array .
+//! By default consider left rotation.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr;
+    arr = {3,4,5,5,7,8,90,1,0};
+
+
+}
